fee1bdcd67d4642a86911a564b7602a377d971ee
adoubleyoueye <awi.mphaphuli@tuta.io>
2022-05-24 23:06:48 +0200

    :tada: "init" commit :tada:

---
+++ pwa/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Jua Jua',
+  email: 'demo@jua.co.za',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+};
+
+export default account;
---
+++ pwa/src/pages/DashboardApp.js
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker'
+import { Grid, Container, Typography } from '@mui/material'
+// components
+import Page from '../components/Page'
+// sections
+import { AppNewsUpdate, AppWidgetSummary } from '../sections/@dashboard/app'
+
+
+export default function DashboardApp() {
+
+  return (
+    <Page title="Dashboard">
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Hi, Welcome back
+        </Typography>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Service Requests" total={4} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Saved Opportunities" total={3} color="info" />
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={8}>
+            <AppNewsUpdate
+              title="News Update"
+              list={[...Array(5)].map((_, index) => ({
+                id: faker.datatype.uuid(),
+                title: faker.name.jobTitle(),
+                description: faker.name.jobTitle(),
+                image: `/static/mock-images/covers/cover_${index + 1}.jpg`,
+                postedAt: faker.date.recent(),
+              }))}
+            />
+          </Grid>
+
+        </Grid>
+      </Container>
+    </Page>
+  )
+}
