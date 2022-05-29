593b0fb0e5d3737d9cc016ce6b1d452267bfe054
adoubleyoueye <awi.mphaphuli@tuta.io>
2022-05-29 21:22:19 +0200

    add user, education and service history form forms

--- pwa/src/pages/Profile.js
+++ pwa/src/pages/Profile.js
@@ -1,15 +1,29 @@
-import { Grid, Container, Typography } from '@mui/material'
-import Page from '../components/Page'
+import { Grid, Container, Typography, Divider } from '@mui/material';
+import EducationHistoryForm from '../sections/@dashboard/app/EducationHistoryForm';
+import Page from '../components/Page';
+import ServiceHistroyForm from '../sections/@dashboard/app/ServiceHistoryForm';
+import UserProfileForm from '../sections/@dashboard/app/UserProfileForm';
 
 export default function Profile() {
-
   return (
     <Page title="Profile">
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }}>
           Profile
+          <Divider />
         </Typography>
+        <Grid>
+          <Grid my={5} />
+          <UserProfileForm />
+          <Grid />
+          <Grid my={5}>
+            <EducationHistoryForm />
+          </Grid>
+          <Grid my={5}>
+            <ServiceHistroyForm />
+          </Grid>
+        </Grid>
       </Container>
     </Page>
-  )
+  );
 }
