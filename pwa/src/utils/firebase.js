8f96060e2bcd12bc0bd56f7fdd52c919460e1244
adoubleyoueye <awi.mphaphuli@tuta.io>
2022-07-19 21:08:51 +0200

    update firebase config to use env

--- pwa/src/utils/firebase.js
+++ pwa/src/utils/firebase.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+/* eslint no-use-before-define: 0 */
+import { initializeApp } from "firebase/app"
+import "firebase/database";
+import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCA_AFOez3-y5C3mt-L6vzBdJZNps5ZxQA",
-  authDomain: "jua-test.firebaseapp.com",
-  projectId: "jua-test",
-  storageBucket: "jua-test.appspot.com",
-  messagingSenderId: "216005444068",
-  appId: "1:216005444068:web:3b496923f6d6d277dbc7f1"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 // export const firebaseDb = getFirestore();
 export const firebaseAuth = getAuth();
