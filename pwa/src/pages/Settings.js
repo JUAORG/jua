4284fd9cc9b50bd0c2deeb9b26d4493fe4b60d40
adoubleyoueye <awi.mphaphuli@tuta.io>
2022-08-23 21:51:13 +0200

    implement basic profile visibiltity feature

--- pwa/src/pages/Settings.js
+++ pwa/src/pages/Settings.js
@@ -1,15 +1,81 @@
-import { Grid, Container, Typography } from '@mui/material'
-import Page from '../components/Page'
+import React, { useState, useEffect } from 'react'
+import { get, map } from 'lodash'
+import {
+  ref,
+  set,
+  child,
+  update,
+  onValue,
+  getDatabase,
+  serverTimestamp
+} from 'firebase/database'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import ListSubheader from '@mui/material/ListSubheader'
+import Switch from '@mui/material/Switch'
+import PersonIcon from '@mui/icons-material/Person'
+import BluetoothIcon from '@mui/icons-material/Bluetooth'
+import { editUserProfile } from '../actions/Profile'
+import { getAuthId } from '../actions/Auth'
+
+const db = getDatabase()
 
 export default function Settings() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    onValue(ref(db, `/users/${getAuthId()}/`), (snapshot) => {  
+      const result = (snapshot.val() && snapshot.val())
+      setUser(result)
+    })
+  }, [db])
+  
+  const handleProfileVisibility = () => {
+    const isProfileVisible = get(user, 'profile_visible')
+    const values = {}
+    if (isProfileVisible) {
+      values.profile_visible = false
+      editUserProfile(values)
+    }else{
+      values.profile_visible = true
+      editUserProfile(values)
+    }
+  }
+
+  const renderProfileVisibilitySetting = () => {
+    return (
+      <ListItem>
+        <ListItemIcon>
+          <PersonIcon />
+        </ListItemIcon>
+        <ListItemText primary='Hide Profile'/>
+        <Switch
+          edge='end'
+          onChange={ handleProfileVisibility }
+          checked={ get(user, 'profile_visible') }
+        />
+      </ListItem>
+    )
+  }
 
   return (
-    <Page title="Profile">
-      <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-            Settings
-        </Typography>
-      </Container>
-    </Page>
+    <List
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper'
+      }}
+      subheader={
+        <ListSubheader>
+          Settings
+        </ListSubheader>
+      }
+    >
+    {user &&
+     renderProfileVisibilitySetting()
+    }
+    </List>
   )
 }
