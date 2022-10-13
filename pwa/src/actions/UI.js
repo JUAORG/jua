2ccbcdec76f8942cfcd47ea272c0986f574d2c58
adoubleyoueye <awi.mphaphuli@tuta.io>
2022-10-13 20:18:18 +0200

    refactor customerViewLogic

--- pwa/src/actions/UI.js
+++ pwa/src/actions/UI.js
@@ -1,11 +1,9 @@
-import { get } from 'lodash'
+import { get, first } from 'lodash'
 
 /* DEV_VIEW should be used to test and change the view for development (customer || arbritary value) */
 
 export const DEV_VIEW = 'customer'
 export const DEFAULT_VIEW = 'customer'
 
-export const showCustomerView = () => {
-  const environment = process.env.NODE_ENV
-  return environment === 'production' && get(window, ['location', 'host']).split('.')[0] === DEFAULT_VIEW || DEV_VIEW === DEFAULT_VIEW
-}
+export const showCustomerView = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ?
+  DEV_VIEW === DEFAULT_VIEW : first(get(window, ['location', 'host']).split('.')) === DEFAULT_VIEW
