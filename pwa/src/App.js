04937da6620412fd4b5770ba4fda481ad68cfcc7
adoubleyoueye <awi.mphaphuli@tuta.io>
2022-10-23 14:23:22 +0200

    add contenful and sentry

--- pwa/src/App.js
+++ pwa/src/App.js
@@ -1,17 +1,41 @@
 import 'react-notifications/lib/notifications.css'
 import { NotificationContainer } from 'react-notifications'
+import * as Sentry from "@sentry/browser"
+import { BrowserTracing } from "@sentry/tracing"
+import { ContentfulClient, ContentfulProvider } from 'react-contentful'
+
 import Router from './routes'
 import ThemeProvider from './theme'
 import ScrollToTop from './components/ScrollToTop'
 
 // ----------------------------------------------------------------------
 
+const contentfulClient = new ContentfulClient({
+  space: process.env.REACT_APP_CONTENTFUL_SPACE,
+  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+})
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+
+
+
 export default function App() {
   return (
     <ThemeProvider>
-      <ScrollToTop />
-      <Router />
-      <NotificationContainer/>
+      <ContentfulProvider client={ contentfulClient }>
+        <ScrollToTop />
+        <Router />
+        <NotificationContainer/>
+      </ContentfulProvider>
     </ThemeProvider>
-  );
+  )
 }
