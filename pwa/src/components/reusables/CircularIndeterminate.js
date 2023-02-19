c20c35703bb43a99b6a07757734bc5c040c95a3d
adoubleyoueye <adoubleyoueye>
2023-02-19 14:07:55 +0200

    fix appearance of loading spinner

--- pwa/src/components/reusables/CircularIndeterminate.js
+++ pwa/src/components/reusables/CircularIndeterminate.js
@@ -1,26 +1,32 @@
-import * as React from 'react'
-import Box from '@mui/material/Box'
-import CircularProgress from '@mui/material/CircularProgress'
-import { makeStyles } from '@mui/styles'
-import Logo from "../Logo"
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+import { makeStyles } from '@mui/styles';
+import Logo from '../Logo';
 
 const useStyles = makeStyles({
   circularLoader: {
     zIndex: 9,
-    top: "40vh",
-    margin: "auto",
-    textAlign: "center",
-    position: "relative"
+    top: '40vh',
+    margin: 'auto',
+    textAlign: 'center',
+    position: 'relative',
   },
 });
 
 export default function CircularIndeterminate() {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
-    <div className={classes.circularLoader}>
-      <CircularProgress sx={{margin: 'auto'}} />
-      <Logo disabledLink sx={{margin: 'auto'}} />
-    </div>
-  )
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={3}
+      className={classes.circularLoader}
+    >
+      <CircularProgress sx={{ margin: 'auto' }} />
+      <Logo disabledLink sx={{ margin: 'auto' }} />
+    </Stack>
+  );
 }
