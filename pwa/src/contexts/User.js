eeabee5b71d346fe53c00fcb2bd2d81d609c29d8
adoubleyoueye <adoubleyoueye>
2023-02-25 22:16:28 +0200

    remove setTimeout

--- pwa/src/contexts/User.js
+++ pwa/src/contexts/User.js
@@ -10,39 +10,39 @@ const UserContext = createContext(undefined);
 const UserDispatchContext = createContext(undefined);
 
 function UserProvider({ children }) {
-  const currentPath = window.location.pathname
-  const [pending, setPending] = useState(true)
-  const [currentUser, setCurrentUser] = useState()
-  const isOnAuthPage = Boolean(currentPath === LOGIN_PATH || currentPath === REGISTER_PATH)
-    
-  useEffect(() => {
-    if (!currentUser) {
-      getUser()
-        .then((response) => {
-          setCurrentUser(response.data);
-            setTimeout(() => setPending(false), 2000)
-        })
-        .catch((err) => {
-          if (get(err, ['response', 'status']) === 401) {
-            if(!isOnAuthPage) {
-              window.location = '/login'
-            }
-            setPending(false)
-          }
-        })
-    }
-  }, [currentUser])
+    const currentPath = window.location.pathname
+    const [pending, setPending] = useState(true)
+    const [currentUser, setCurrentUser] = useState()
+    const isOnAuthPage = Boolean(currentPath === LOGIN_PATH || currentPath === REGISTER_PATH)
+
+    useEffect(() => {
+        if (!currentUser) {
+            getUser()
+                .then((response) => {
+                    setCurrentUser(response.data);
+                    setPending(false)
+                })
+                .catch((err) => {
+                    if (get(err, ['response', 'status']) === 401) {
+                        if(!isOnAuthPage) {
+                            window.location = '/login'
+                        }
+                        setPending(false)
+                    }
+                })
+        }
+    }, [currentUser])
 
 
-  return (
-      <>
-      {pending && <CircularIndeterminate /> }
-      {!pending && <UserContext.Provider value={currentUser}>
-       <UserDispatchContext.Provider value={setCurrentUser}>{children}</UserDispatchContext.Provider>
-       </UserContext.Provider>
-      }
-      </>
-  )
+    return (
+            <>
+              {pending && <CircularIndeterminate /> }
+              {!pending && <UserContext.Provider value={currentUser}>
+                             <UserDispatchContext.Provider value={setCurrentUser}>{children}</UserDispatchContext.Provider>
+                           </UserContext.Provider>
+              }
+            </>
+    )
 }
 
 export { UserProvider, UserContext, UserDispatchContext }
