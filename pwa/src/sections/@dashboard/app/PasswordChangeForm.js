a9f1e354174d8362fdd26136aa329491ab8b3fd7
adoubleyoueye <adoubleyoueye>
2023-02-26 16:17:24 +0200

    connect password change form to api

--- pwa/src/sections/@dashboard/app/PasswordChangeForm.js
+++ pwa/src/sections/@dashboard/app/PasswordChangeForm.js
@@ -1,46 +1,45 @@
-import react, {useEffect, useState} from 'react'
-import { get, head } from 'lodash'
 import { useForm } from "react-hook-form"
+import { useMutation, useQueryClient } from 'react-query'
 import { Stack, TextField } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
-import { createId } from '../../../utils/uuid-generator'
-import { getUser } from '../../../actions/Auth'
+import { updateUserPassword } from '../../../actions/Auth'
+import notificationManager from '../../../actions/NotificationManager'
 
 export default function PasswordChangeForm() {
-  const [userProfile, setUserProfile] = useState(null)
-  const formProps = useForm({ defaultValues: userProfile })
-  
-  const {
-    reset,
-    watch,
-    control,
-    setValue,
-    register,
-    getValues,
-    handleSubmit,
-    formState: { errors },
-  } = formProps
+  const queryClient = useQueryClient();
+  const formProps = useForm({ defaultValues: null });
+  const { register, reset, handleSubmit } = formProps;
 
-  const onSubmit = (values) => {
+  const { mutate } = useMutation({
+    mutationFn: (values) => updateUserPassword(values),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+      notificationManager.success('Password updated', 'Success');
+      reset();
+    },
+    onError: () => notificationManager.error('Something went wrong.', 'Error')
+  });
 
-  }
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit((values) => mutate(values))}>
       <Stack spacing={3}>
         <TextField
+          required
           fullWidth
           type="password"
           label="Current Password"
-          {...register('current_password')}
+          {...register('old_password')}
         />
         <TextField
+          required
           fullWidth
           type="password"
           label="New Password"
           {...register('new_password')}
         />
         <TextField
+          required
           fullWidth
           type="password"
           label="Confirm New Password"
