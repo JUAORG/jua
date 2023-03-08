3a5aeb9b5376662e5b59ab49f3b87a68b6e66d11
adoubleyoueye <adoubleyoueye>
2023-03-08 23:05:02 +0200

    add transaction ledger

--- pwa/src/actions/Wallet.js
+++ pwa/src/actions/Wallet.js
@@ -1,39 +1,40 @@
-import {
-  map,
-  get,
-  size,
-  merge,
-  filter,
-} from 'lodash'
-import notificationManager from './NotificationManager'
-import { createId } from '../utils/uuid-generator'
-import { getAuthId } from './Auth'
-
+import axios from 'axios';
+import { map, get } from 'lodash';
+import notificationManager from './NotificationManager';
+import { createId } from '../utils/uuid-generator';
+import { defaultHeaders } from './Auth';
 
 export const yoco = new window.YocoSDK({
   publicKey: process.env.REACT_APP_YOCO_PUBLIC_KEY,
-})
+});
 
 export async function creditJuaWallet(amount) {
-  const transactionId = createId()
-  const values = {}
-  values.id = transactionId
-  values.action = 'credit'
-  values.amount = parseFloat(amount)
-
- 
+  const transactionId = createId();
+  const values = {};
+  values.id = transactionId;
+  values.action = 'credit';
+  values.amount = parseFloat(amount);
 }
 
 export async function getAvailableFunds(ledgerRecords) {
-  let total = 0
+  let total = 0;
   const bb = map(ledgerRecords, (x) => {
-    total += get(x, 'amount')
-  })
-  return total
+    total += get(x, 'amount');
+  });
+  return total;
+}
+
+export async function fetchUserLedger() {
+  return axios({
+    method: 'GET',
+    url: `${process.env.REACT_APP_API_BASE_URL}/api/user_profile_ledger/`,
+    withCredentials: false,
+    headers: defaultHeaders,
+  });
 }
 
 export async function makePayment(amount) {
-  const amountInCents = amount * 100
+  const amountInCents = amount * 100;
   yoco.showPopup({
     amountInCents,
     currency: 'ZAR',
@@ -43,25 +44,28 @@ export async function makePayment(amount) {
       // This function returns a token that your server can use to capture a payment
       if (result.error) {
         const errorMessage = result.error.message;
-        notificationManager.error(`${result.error.message}', 'Error`)
+        notificationManager.error(`${result.error.message}', 'Error`);
         alert(`error occured: ${errorMessage}`);
       } else {
-        fetch(process.env.REACT_APP_PAYMENT_ENDPOINT, {
-          body: `${get(result, 'id')},${amountInCents}`,
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          method: "POST"
-        }).then((res) => {
-          const statusCode = res.status
-          if (statusCode === 200 || statusCode === 201) {
-           creditJuaWallet(amount).then(() => {
-             notificationManager.success(`R${amount} credited to your JUA wallet', 'Success`)
-            })
-          }
-        }).catch(() => {
-        notificationManager.error('Something went wrong', 'Error')
-      })
-    }
-  }
-})}
+        axios({
+          method: 'POST',
+          url: process.env.REACT_APP_PAYMENT_ENDPOINT,
+          withCredentials: false,
+          headers: defaultHeaders,
+          data: {amountInCents, token: get(result, 'id')},
+        })
+          .then((res) => {
+            const statusCode = res.status;
+            if (statusCode === 200 || statusCode === 201) {
+              creditJuaWallet(amount).then(() => {
+                notificationManager.success(`R${amount} credited to your JUA wallet', 'Success`);
+              });
+            }
+          })
+          .catch(() => {
+            notificationManager.error('Something went wrong', 'Error');
+          });
+      }
+    },
+  });
+}
