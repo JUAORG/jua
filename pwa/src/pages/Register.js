4b1d5a0e76af73403aa07ffa9d5f5b157c16c41b
Awi Mphaphuli <awi.mphaphuli@tuta.io>
2023-09-21 20:56:33 +0200

    Update Register.js

--- pwa/src/pages/Register.js
+++ pwa/src/pages/Register.js
@@ -109,6 +109,11 @@ export default function Register() {
               </Link>
               .
             </Typography>
+            <p>
+                JUA Advisory is still in development mode. 
+                We are currently user testing the platform and adding final touches. 
+                Please feel free to register/sign in, experience the app, and give us feedback.
+              </p>
 
             {!smUp && (
               <Typography variant="body2" sx={{ mt: 3, textAlign: 'center' }}>
